ca5afb91b12c770aa3c83001d9c4ac7bf6d114e1
Dave Hudson <dave-hudson@users.noreply.github.com>
2024-06-13 21:38:33 +0100

    Move Google Tag Manager script
    * Move the Google Tag Manager script code to after the site renders.
      This significantly improves performance on PageSpeed.

--- src/app.js
+++ src/app.js
@@ -133,6 +133,15 @@ function routeInit() {
     };
 
     handleLocation();
+
+    // Load up the Google Tag Manager.
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', 'G-5NG75PCCCR');
 }
 
 document.addEventListener('DOMContentLoaded', routeInit());
