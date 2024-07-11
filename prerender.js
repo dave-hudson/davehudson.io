ae19cd76f3838cbe63c9034485d8be5870a42d11
Dave Hudson <dave-hudson@users.noreply.github.com>
2024-07-11 08:00:26 +0100

    Improve pre-rendering
    * Expand parallel pre-renders to 16.
    
    * Ensure we delete any target file before creating it so we don't pick
      up old pre-rendered files instead of generating from the new ones.

--- prerender.js
+++ prerender.js
@@ -8,7 +8,7 @@ const sitemapPath = './build/sitemap.xml';
 
 const outputDir = './build';
 const localBaseUrl = 'http://localhost:3000';
-const maxConcurrentRenders = 8;
+const maxConcurrentRenders = 16;
 
 /**
  * Utility function to create directories recursively
@@ -35,14 +35,25 @@ const ensureDirectoryExistence = (filePath) => {
 
     // Helper function to render a single page
     const renderPage = async (url) => {
+        // Before we attempt to render the file, ensure we have the target directory ready.
+        const urlPath = new URL(url).pathname;
+        const filePath = path.join(outputDir, urlPath, 'index.html');
+        ensureDirectoryExistence(filePath);
+
+        // If the target file already exists then delete it!  We do this because the target file will be
+        // the one served up by our webserver so we'll end up re-rendering it instead of the new content.
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+        }
+
+        // Render the page and generate HTML.
         const localURL = url.replace(/^https?:\/\/[^\/]+/, localBaseUrl);
         console.log(`Processing URL: ${localURL}`)
         const page = await browser.newPage();
         await page.goto(localURL, { waitUntil: 'networkidle0' });
         const html = await page.content();
-        const urlPath = new URL(url).pathname;
-        const filePath = path.join(outputDir, urlPath, 'index.html');
-        ensureDirectoryExistence(filePath);
+
+        // Finally, write the HTML to a file.
         fs.writeFileSync(filePath, html);
         await page.close();
     };
