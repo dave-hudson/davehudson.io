c18aa8d1c25ba01739a7a7dd0092d634d95a8ee7
Dave Hudson <dave-hudson@users.noreply.github.com>
2024-08-07 21:06:31 +0100

    Update makefile

--- src/blog/2024-08-06-0700/Makefile.mk
+++ src/blog/2024-08-06-0700/Makefile.mk
@@ -2,6 +2,6 @@ FILES += \
 	src/blog/2024-08-06-0700/prompt.txt \
 	src/blog/2024-08-06-0700/siterender.ts \
 	src/blog/2024-08-06-0700/logic.ts \
-	src/blog/2024-08-06-0700/robot.webp
+	src/blog/2024-08-06-0700/dave.png
 
 TS_FILES += src/blog/2024-08-06-0700/2024-08-06-0700.ts
